b5c7c0d8cc307178edad065a17ec5dfa03b4bf3e
bsalunke-qlys <bsalunke@qualys.com>
2020-06-08 12:04:07 -0700

    init commit

---
+++ alpine/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+## Process tar files
+import tarfile
+import os
+import sys
+import time
+import numpy as np
+
+## Json data parsing
+import json
+
+print np.power(100, 8, dtype=np.int64)
